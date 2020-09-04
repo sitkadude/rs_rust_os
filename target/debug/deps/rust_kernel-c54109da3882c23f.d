525b0299fd9e8f161b24b98c72416ba64bb379bb
Lukas Jorgensen <lukas.r.jorgensen@gmail.com>
2020-09-04 13:44:19 -0400

    created binary executable and removed std library

---
+++ target/debug/deps/rust_kernel-c54109da3882c23f.d
@@ -0,0 +1,5 @@
+/home/lukasjorgensen/CODE/sitkadude/rs_rust_kernel/target/debug/deps/rust_kernel-c54109da3882c23f: src/main.rs
+
+/home/lukasjorgensen/CODE/sitkadude/rs_rust_kernel/target/debug/deps/rust_kernel-c54109da3882c23f.d: src/main.rs
+
+src/main.rs:
